d5217dd85750013ab10183ce08a3e0e3b38290f1
Sivakar Sithamparanathan <sivakar12@outlook.com>
2024-05-09 13:02:55 +0530

    Use shared components in customer-frontend

--- packages/customer-frontend/src/index.tsx
+++ packages/customer-frontend/src/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Footer } from 'shared';
 
 const App = () => {
-  return <h1>Customer Frontend App</h1>;
+  return (
+    <div>
+      <h1>Customer Frontend App</h1>
+      <Footer />
+    </div>
+  
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
